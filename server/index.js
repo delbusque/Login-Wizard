559e0c1e4c0ecfa51f8cbe17adc7a98ae13eeea9
Ventsislav Peychev <del.busque@gmail.com>
2023-05-18 11:40:21 +0300

    Add server validation and generate 6-digit code. Save email, mobile No and code in server session

--- server/index.js
+++ server/index.js
@@ -1,11 +1,55 @@
+require('dotenv').config();
+
 const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const crypto = require('crypto');
+
+const cookieParser = require('cookie-parser');
+const { log } = require('console');
 
 const app = express();
 
+app.use(session({
+    secret: 'come on coys',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+app.use(cookieParser());
+
 app.use(express.json());
 
-app.use('/test', (req, res) => {
-    res.status(200).json({ mssg: 'Test passed successfully !' })
+app.post('/auth', (req, res) => {
+    const { mobileNo, email } = req.body;
+
+    if (!mobileNo || !email) {
+        return res.status(401).json({ mssg: 'You must provide Mobile no. and Email !' })
+    }
+
+    if (!validator.isEmail(email)) {
+        return res.status(401).json({ mssg: 'Invalid email format !' })
+    }
+
+    // if (userSessions[email]) {
+    //     res.status(400).send('User with that email already exists !')
+    // }
+
+    crypto.randomBytes(3, function (err, buffer) {
+        const code = parseInt(buffer.toString('hex'), 16).toString().substr(0, 6);
+
+        req.session.userSession = { email, mobileNo, code };
+
+        res.status(200).json({ code });
+    });
 })
 
-app.listen(5000, () => console.log('Server is listening for requests on port 5000 ...'));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening for requests on port ${process.env.PORT}...`)
+        })
+    })
+    .catch(error => console.log(error))
