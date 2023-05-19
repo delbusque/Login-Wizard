418fbc7c9bbd9ab81826c926f366d84969b4f97b
delbusque <del.busque@gmail.com>
2023-05-19 15:02:00 +0300

    Added log row component with dynamic data

---
+++ client/src/components/LogRow/LogRow.js
@@ -0,0 +1,25 @@
+import styles from './LogRow.module.css';
+
+const LogRow = ({ log }) => {
+    return (
+        <div className={styles['table-row-cont']}>
+
+            <div className={styles['row-owner']}>{log.email}</div>
+
+            {log.status === 'correct' ?
+                <div className={styles['row-status']}>{log.status}</div> :
+                <div className={styles['row-status-error']}>{log.status}</div>
+            }
+
+            <div className={styles['row-code']}>{log.userCode}</div>
+            <div className={styles['row-timestamp']}>{log.timestamp}</div>
+            <div className={styles['row-buttons']}>
+                <span className={styles['row-pencil']}><i className="fa-solid fa-pencil"></i></span>
+                <span className={styles['row-trash']}><i className="fa-solid fa-trash"></i></span>
+            </div>
+
+        </div>
+    )
+}
+
+export default LogRow;
