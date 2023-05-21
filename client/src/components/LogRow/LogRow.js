cc9f568dd549309fc754e708cb1f0eb539f57929
Ventsislav Peychev <del.busque@gmail.com>
2023-05-21 20:52:32 +0300

    Added delete log handler

--- client/src/components/LogRow/LogRow.js
+++ client/src/components/LogRow/LogRow.js
@@ -1,6 +1,20 @@
 import styles from './LogRow.module.css';
 
-const LogRow = ({ log }) => {
+const LogRow = ({ log, logs, setLogs }) => {
+
+    const deleteHandler = () => {
+        const newLogs = logs.filter(l => l.id !== log.id);
+        setLogs(newLogs)
+        fetch('/logs', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ logId: log.id })
+        })
+    }
+
+
     return (
         <div className={styles['table-row-cont']}>
 
@@ -15,7 +29,7 @@ const LogRow = ({ log }) => {
             <div className={styles['row-timestamp']}>{log.timestamp}</div>
             <div className={styles['row-buttons']}>
                 <span className={styles['row-pencil']}><i className="fa-solid fa-pencil"></i></span>
-                <span className={styles['row-trash']}><i className="fa-solid fa-trash"></i></span>
+                <span className={styles['row-trash']} onClick={deleteHandler}><i className="fa-solid fa-trash"></i></span>
             </div>
 
         </div>
